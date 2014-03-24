ff67f03362757d9b045e3f2241d66e4f3bb614e3
Martin van Harmelen <Martin@vanharmelen.com>
2014-03-24 17:07:25 +0100

    Changed board size calculations and...
    - added lots of constants
    - updated draw_turn functionality
    - removed old code
    - fine tuned circle drawing
    - fine tuned highlighting

--- board.py
+++ board.py
@@ -1,30 +1,37 @@
 import math
 import pygame
 from pygame.locals import *
+import warnings
 
-CIRCLE = 0
-CROSS = 1
 DEBUG = []
 DEBUG_LJUST = 15
 
 class Piece(object):
-    def __init__(self, name, color, thickness=2):
+    def __init__(self, name, abbr, color, thickness=2):
         self.name = name
+        self.abbr = abbr
         self.color = color
         self.thickness = thickness
+
+    def __repr__(self):
+        return '{}'.format(self.abbr)
 
     def draw(self, surface, pos, size):
         """Draws the representation of a piece."""
         raise NotImplemented('You must write this for each piece.')
 
+
+
 class Nought(Piece):
     def __init__(self, color):
         name = 'nought'
+        abbr = 'O'
         self.size = 0.9
-        super(Nought, self).__init__(name, color)
+        super(Nought, self).__init__(name, abbr, color)
         if 'Nought' in DEBUG:
             print('Nought.__init__')
             print(self.thickness, type(self.thickness))
+
 
     def draw(self, surface, pos, size):
         """Draws the representation of a Nought."""
@@ -34,7 +41,7 @@ class Nought(Piece):
             print('Pos'.ljust(DEBUG_LJUST), pos, type(pos), [type(x) for x in pos])
             print('Size'.ljust(DEBUG_LJUST), size, type(size))
             print('Thickness'.ljust(DEBUG_LJUST), self.thickness, type(self.thickness))
-        pos = (int(pos[0] + size/2), int(pos[1] + size/2))
+        pos = (math.ceil(pos[0] + size/2), math.ceil(pos[1] + size/2))
         pygame.draw.circle(
             surface,
             self.color,
@@ -46,8 +53,10 @@ class Nought(Piece):
 class Cross(Piece):
     def __init__(self, color):
         name = 'cross'
+        abbr = 'X'
         self.margin = 4
-        super(Cross, self).__init__(name, color)
+        super(Cross, self).__init__(name, abbr, color)
+
 
     def draw(self, surface, pos, size):
         """Draws the representation of a Cross."""
@@ -74,7 +83,8 @@ class Cross(Piece):
 
 
 class Board(object):
-    def __init__(self, pieces, size_px, margin_px, colors, font='courier new', n_rows=3):
+    def __init__(self, pieces, tile_size, line_thickness, margin, design, n_rows=3):
+        # Verify pieces
         self.pieces = []
         for piece in pieces:
             if isinstance(piece, Piece):
@@ -84,41 +94,60 @@ class Board(object):
                                 '(a subclass of) Piece.')
         self.turn = 0
 
-        self.real_size, self.margin = size_px, margin_px
-        self.size, self.tile_size = size_px-margin_px, (size_px-margin_px)/n_rows**2
-        self.colors = colors
+        # Calculate board size
+        if not tile_size % 2:
+            warnings.warn('The design of the board is best with an odd tile_size.')
+
+        self.tile_size = tile_size
+        self.line_thickness = line_thickness
+        self.tile_line_size = tile_size + 2 * line_thickness
+        self.margin = margin
         self.n_rows = n_rows
 
-        self.tiles = [[None]*n_rows**2 for i in range(n_rows**2)]
+        for name in ['tile_size', 'line_thickness', 'margin', 'n_rows']:
+            attr = getattr(self, name)
+            if not isinstance(attr, int):
+                raise TypeError('{} must be an int, got {}.'.format(name, attr))
 
-        self.outer_surface = pygame.Surface((self.real_size, self.real_size))
-        self.outer_surface.fill(colors['background'])
+        self.inner_size = n_rows**2 * self.tile_line_size
+        self.outer_size = self.inner_size + margin * 2
+
+        self.design = design
+        self.reset()
+
+
+    def pygame_init(self):
+        self.outer_surface = pygame.Surface([self.outer_size]*2)
+        self.outer_surface.fill(self.design['background-color'])
 
         # Draw the row and column numbers.
-        font = pygame.font.Font(pygame.font.match_font(font), 16)
-        for i in range(n_rows**2):
-            f = font.render(str(i), False, self.colors['border'])
+        font = pygame.font.Font(pygame.font.match_font(self.design['font-name']), self.design['font-size'])
+        for i in range(self.n_rows**2):
+            f = font.render(str(i), False, self.design['text-color'])
             rect = f.get_rect()
             rect.topleft = (
-                self.tile_size*(i+1)-rect.width/2-self.tile_size/2+self.margin,
+                self.tile_line_size*(i+1)-rect.width/2-self.tile_line_size/2+self.margin,
                 self.margin/2-rect.height/2
             )
             self.outer_surface.blit(f, rect)
             rect.topleft = (
                 self.margin/2-rect.width/2,
-                self.tile_size*(i+1)-rect.height/2-self.tile_size/2+self.margin
+                self.tile_line_size*(i+1)-rect.height/2-self.tile_line_size/2+self.margin
             )
             self.outer_surface.blit(f, rect)
 
-        self.surface = pygame.Surface((self.size, self.size))
+        self.surface = pygame.Surface([self.inner_size]*2)
         self.draw_board()
 
-    def switch_turns(self):
-        self.turn = (self.turn + 1) % len(self.pieces)
+
+    def reset(self):
+        self.tiles = [[None]*self.n_rows**2 for i in range(self.n_rows**2)]
+
 
     def draw_board(self):
         """Draw the board to the surface, with everything on it."""
-        self.surface.fill(self.colors['background'])
+        self.surface.fill(self.design['background-color'])
+
         for x in range(self.n_rows**2):
             for y in range(self.n_rows**2):
                 # First draw the tile itself, which is just some borders.
@@ -126,25 +155,33 @@ class Board(object):
                 if 'pos' in DEBUG:
                     print('Board.draw_board')
                     print('pos'.ljust(DEBUG_LJUST), pos)
-                rect = pygame.Rect(pos, [self.tile_size]*2)
-                pygame.draw.rect(self.surface, self.colors['border'], rect, 1)
+                rect = pygame.Rect(pos, [self.tile_line_size]*2)
+                pygame.draw.rect(
+                    self.surface,
+                    self.design['small-border-color'],
+                    rect,
+                    self.line_thickness
+                )
 
                 # Then draw a piece in it, if necessary.
                 tile = self.tiles[x][y]
+                pos = (pos[0] + self.line_thickness, pos[1] + self.line_thickness)
                 if tile is not None:
                     tile.draw(self.surface, pos, self.tile_size)
 
         # Now draw the four "big" lines on the board.
-        lines = (
-            ((self.tile_size*3-1, 0), (self.tile_size*3-1, self.size)),
-            ((self.tile_size*6-1, 0), (self.tile_size*6-1, self.size)),
-            ((0, self.tile_size*3-1), (self.size, self.tile_size*3-1)),
-            ((0, self.tile_size*6-1), (self.size, self.tile_size*6-1)),
-        )
-        for line in lines:
-            pygame.draw.line(self.surface, self.colors['big'], line[0], line[1], 2)
+        for n in range(1, self.n_rows):
+            start = self.n_rows*self.tile_line_size*n - self.line_thickness
+            lines = [
+                ((start, 0), (start, self.inner_size)),
+                ((0, start), (self.inner_size, start))
+            ]
 
-        self.outer_surface.blit(self.surface, (self.margin, self.margin))
+            for line in lines:
+                pygame.draw.line(self.surface, self.design['big-border-color'],
+                                 line[0], line[1], self.line_thickness * 2)
+
+        self.outer_surface.blit(self.surface, [self.margin]*2)
 
 
     def coords_to_pos(self, coords):
@@ -153,17 +190,21 @@ class Board(object):
             print('Board.coords_to_pos')
             print('Coords'.ljust(DEBUG_LJUST), coords, type(coords), [type(x) for x in coords])
         x, y = coords
-        return (x)*self.tile_size, (y)*self.tile_size
+        return (x)*self.tile_line_size, (y)*self.tile_line_size
 
 
     def pos_to_coords(self, pos):
         """Take pixel positions and turn them into coordinates (from 0 to 8)."""
         x, y = pos
-        coords = (math.floor(x/self.tile_size), math.floor(y/self.tile_size))
+        coords = (math.floor(x/self.tile_line_size), math.floor(y/self.tile_line_size))
         if 'pos_to_coords' in DEBUG:
             print('Board.pos_to_coords')
             print('Coords'.ljust(DEBUG_LJUST), coords, type(coords), [type(x) for x in coords])
         return coords
+
+
+    def switch_turns(self):
+        self.turn = (self.turn + 1) % len(self.pieces)
 
 
     def make_a_move(self, coords):
@@ -196,12 +237,28 @@ class Board(object):
         # But, don't highlight any tiles that have a value!
         if self.tiles[coords[0]][coords[1]] is not None:
             return
-        highlight = pygame.Surface((self.size, self.size), pygame.SRCALPHA)
+        highlight = pygame.Surface([self.inner_size]*2, pygame.SRCALPHA)
 
         # What's this?
-        x, y = map(lambda i: i+1, self.coords_to_pos(coords))
-        rect = pygame.Rect((x, y), (self.tile_size-2,)*2)
-        pygame.draw.rect(highlight, self.colors['highlight'], rect, 0)
+        x, y = map(lambda i: i + self.line_thickness, self.coords_to_pos(coords))
+        rect = pygame.Rect((x, y), (self.tile_size,)*2)
+        pygame.draw.rect(highlight, self.design['highlight-color'], rect, 0)
 
-        self.surface.blit(highlight, (0, 0))
-        self.outer_surface.blit(self.surface, (self.margin, self.margin))
+        self.surface.blit(highlight, [0]*2)
+        self.outer_surface.blit(self.surface, [self.margin]*2)
+
+
+    def get_size(self):
+        return self.outer_size
+
+
+    def pos_in_board(self, pos):
+        x = pos[0] - self.margin
+        y = pos[1] - self.margin
+        if 0 < x < self.inner_size and 0 < y < self.inner_size:
+            return x, y
+        return False
+
+
+    def get_turn_text(self):
+        return str(self.pieces[self.turn])
--- game.py
+++ game.py
@@ -3,76 +3,83 @@ from pygame.locals import *
 
 import board
 
-WINDOW_SIZE = (800, 680)
-BOARD_SIZE = (640, 640)
+TILE_SIZE = 99
+LINE_THICKNESS = 1
 MARGIN = 20
 BACKGROUND_COLOR = (255, 255, 255)
+TEXT_COLOR = (0, 0, 0)
+CROSS_COLOR = (0, 0, 255)
+NOUGHT_COLOR = (0, 155, 0)
 PROGRAM_NAME = 'Mega3T'
 FONT = 'courier new'
+FONT_SIZE = 16
 
-def in_board(pos):
-    if MARGIN < event.pos[0] < BOARD_SIZE[0]+MARGIN and MARGIN < event.pos[1] < BOARD_SIZE[1]+MARGIN:
-        return event.pos[0]-MARGIN, event.pos[1]-MARGIN
-    return False
+BOARD_DESIGN = {
+    'background-color'  :   BACKGROUND_COLOR,
+    'small-border-color':   (0, 0, 0),
+    'big-border-color'  :   (255, 0, 0),
+    'highlight-color'   :   pygame.Color(0, 0, 0, 32),
+    'font-name'         :   FONT,
+    'font-size'         :   FONT_SIZE,
+    'text-color'        :   TEXT_COLOR
+}
+
+
+b = board.Board(
+        [board.Cross(CROSS_COLOR), board.Nought(NOUGHT_COLOR)],
+        TILE_SIZE,
+        LINE_THICKNESS,
+        MARGIN,
+        BOARD_DESIGN
+    )
+
+window_size = (b.get_size() + 160, b.get_size())
 
 pygame.init()
-window = pygame.display.set_mode(WINDOW_SIZE)
+window = pygame.display.set_mode(window_size)
 window.fill(BACKGROUND_COLOR)
 pygame.display.set_caption(PROGRAM_NAME)
 
 # Render and blit title, and turn.
 bold = pygame.font.Font(pygame.font.match_font(FONT, bold=True), 21)
-title = bold.render(PROGRAM_NAME, False, (0, 0, 0))
-window.blit(title, (680, MARGIN))
+title = bold.render(PROGRAM_NAME, False, TEXT_COLOR)
+window.blit(title, (b.get_size(), MARGIN))
 
-font = pygame.font.Font(pygame.font.match_font(FONT), 16)
-turn_text = font.render('', False, (0, 0, 0))
-def draw_turn(turn):
+font = pygame.font.Font(pygame.font.match_font(FONT), FONT_SIZE)
+turn_text = font.render('', False, TEXT_COLOR)
+def draw_turn(turn_str):
     """Override the turn text to show who's turn it is."""
     # omg a global!? Yeah I'm sorry, I got lazy. TODO!
     global turn_text
 
     # Draw a white rectangle over the old text, then write a new text!
-    override_rect = pygame.Rect((680, MARGIN+title.get_rect().height), turn_text.get_rect().size)
-    pygame.draw.rect(window, (255, 255, 255), override_rect)
-    turn_str = 'Turn: X' if turn else 'Turn: O'
-    turn_text = font.render(turn_str, False, (0, 0, 0))
-    window.blit(turn_text, (680, MARGIN+title.get_rect().height))
+    override_rect = pygame.Rect((b.get_size(), MARGIN+title.get_rect().height), turn_text.get_rect().size)
+    pygame.draw.rect(window, BACKGROUND_COLOR, override_rect)
+    turn_str = 'Turn: ' + turn_str
+    turn_text = font.render(turn_str, False, TEXT_COLOR)
+    window.blit(turn_text, (b.get_size(), MARGIN+title.get_rect().height))
 
-b = board.Board(
-        [board.Cross((0, 0, 255)), board.Nought((0, 155, 0))],
-        BOARD_SIZE[0]+MARGIN,
-        MARGIN,
-        {
-         'background': (255, 255, 255),
-         'border': (0, 0, 0),
-         'big': (255, 0, 0),
-         'highlight': pygame.Color(0, 0, 0, 32),
-         'circle': (0, 155, 0),
-         'cross': (0, 0, 255)
-        }
-    )
+b.pygame_init()
 window.blit(b.outer_surface, (0, 0))
 pygame.display.update()
 
-turn = board.CROSS
-draw_turn(turn)
+draw_turn(b.get_turn_text())
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == MOUSEMOTION:
-            pos = in_board(event.pos)
+            pos = b.pos_in_board(event.pos)
             if pos:
                 b.highlight_tile(b.pos_to_coords(pos))
+            else:
+                b.draw_board()
         elif event.type == MOUSEBUTTONUP:
-            pos = in_board(event.pos)
+            pos = b.pos_in_board(event.pos)
             if pos:
                 if b.make_a_move(b.pos_to_coords(pos)):
-                    turn = not turn
-
-                    draw_turn(turn)
+                    draw_turn(b.get_turn_text())
 
     window.blit(b.outer_surface, (0, 0))
     pygame.display.update()
