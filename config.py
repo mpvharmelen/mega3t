4ad9bba2c6e78469dc82dd86621014d203718f25
Martin van Harmelen <Martin@vanharmelen.com>
2015-07-24 00:05:53 -0400

    Improved comments in config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
-# Regular configuration
-from constants import *
+from constants import CROSS_COLOR, NOUGHT_COLOR
 
-# Debugging configuration
-import logging
+########################### Debugging configuration ############################
+# Set this to True to enable forcing moves. If FORCE_MOVE then the possibility
+# of forcing moves can be toggled by pressing constants.FORCE_KEY
 FORCE_MOVE = False
+
+import logging
 GAME_LOGGING_LEVEL = logging.WARN
 BOARD_LOGGING_LEVEL = logging.WARN
 PIECES_LOGGING_LEVEL = logging.WARN
 
+
+
+############################## Game configuration ##############################
 # This is at the end of the file to avoid circular dependency
 import pieces
 # This is a function to avoid circular dependency
