52ae3d7e958de845de881b3cadad3480b3af6121
Martin van Harmelen <Martin@vanharmelen.com>
2015-07-23 21:12:28 -0400

    Improved comments & moved ai to pieces.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ PIECES_LOGGING_LEVEL = logging.INFO
 # This is at the end of the file to avoid circular dependency
 # from ai import CrossAI, NoughtAI
 # from pieces import Cross, Nought
-import ai
+import pieces
 # This is a function to avoid circular dependency
 def get_pieces():
     """The pieces to be used in the game"""
-    return ai.Cross(CROSS_COLOR), ai.NoughtAI(NOUGHT_COLOR)
+    return pieces.Cross(CROSS_COLOR), pieces.NoughtAI(NOUGHT_COLOR)
--- pieces.py
+++ pieces.py
@@ -8,12 +8,26 @@ class AIMixin(object):
     def save_board_info(self, n_rows, pieces):
         """
         Save the info of the board to the AI object, so it can be used for
-        the algorithm
+        the algorithm. Called once, before the first call to move.
+
+        The return value is ignored.
         """
         return NotImplemented
 
-    def move(self, mutations):
-        """Make a move"""
+    def move(self, mutations, allowed_moves):
+        """
+        Make a move
+
+        Must return a (x, y) tuple of the coordinates of the next move.
+        (0, 0) is top left,
+        (board.n_rows ** 2, board.n_rows ** 2) is bottom right.
+
+        mutations is a list of (piece, (x, y)) tuples of the moves made
+        after your last move.
+
+        allowed_moves is a list of coordinates showing you the squares you are
+        allowed to play.
+        """
         return NotImplemented
 
 
@@ -28,7 +42,12 @@ class Piece(object):
         return '{}'.format(self.abbr)
 
     def draw(self, surface, pos, size):
-        """Draws the representation of a piece."""
+        """
+        Draw the representation of a piece on the given surface in a square of
+        size * size with its top left corner at pos.
+
+        The return value is ignored.
+        """
         return NotImplemented
 
     def is_AI(self):
@@ -94,3 +113,20 @@ class Cross(Piece):
             (pos[0]+self.margin, pos[1]+size-self.margin),
             self.thickness
         )
+
+import random
+class RandomAI(AIMixin):
+    def __init__(self):
+        self.n_rows = None
+
+    def save_board_info(self, n_rows, pieces):
+        self.n_rows = n_rows
+
+    def move(self, mutations, allowed_moves):
+        return random.choice(allowed_moves)
+
+class NoughtAI(Nought, RandomAI):
+    pass
+
+class CrossAI(Cross, RandomAI):
+    pass
