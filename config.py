c964656888bf5124e254d149b6f49edf17cf427e
Martin van Harmelen <Martin@vanharmelen.com>
2015-07-24 00:00:59 -0400

    Changed logging levels

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from constants import *
 # Debugging configuration
 import logging
 FORCE_MOVE = False
-GAME_LOGGING_LEVEL = logging.INFO
-BOARD_LOGGING_LEVEL = logging.INFO
-PIECES_LOGGING_LEVEL = logging.INFO
+GAME_LOGGING_LEVEL = logging.WARN
+BOARD_LOGGING_LEVEL = logging.WARN
+PIECES_LOGGING_LEVEL = logging.WARN
 
 # This is at the end of the file to avoid circular dependency
-# from ai import CrossAI, NoughtAI
-# from pieces import Cross, Nought
 import pieces
 # This is a function to avoid circular dependency
 def get_pieces():
